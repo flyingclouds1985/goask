5cdb208a3a894a5a6e277c8bb081befc79cd0543
alireza <infosoftwareha@yahoo.com>
2018-06-14 03:32:42 +0430

    Complete comments api.

--- api/comment.go
+++ api/comment.go
@@ -7,8 +7,11 @@ import (
 	"github.com/gin-gonic/gin"
 )
 
+// GetQuestionCommentList returns a list consists of comments for the question.
 func (a *Api) GetQuestionCommentList(c *gin.Context) {
-	list, err := a.Store.CommentList(c.Request.URL.Query())
+	query := c.Request.URL.Query()
+	query.Set("question_id", c.Param("question_id"))
+	list, err := a.Store.QuestionCommentList(query)
 
 	if err != nil {
 		JSONBadRequestError("Error in getting comments list. ", err, c)
@@ -17,6 +20,7 @@ func (a *Api) GetQuestionCommentList(c *gin.Context) {
 	c.JSON(200, list)
 }
 
+// PostQuestionComment creates a comment for the question.
 func (a *Api) PostQuestionComment(c *gin.Context) {
 	// claims := jwt.ExtractClaims(c)
 	in := new(model.Comment)
@@ -31,17 +35,44 @@ func (a *Api) PostQuestionComment(c *gin.Context) {
 	// comment.AuthorID = claims["id"]
 	qid, _ := strconv.Atoi(c.Param("question_id"))
 
-	if err = a.Store.CommentCreate(comment, qid); err != nil {
+	if err = a.Store.QuestionCommentCreate(comment, qid); err != nil {
 		JSONBadRequestError("Error in inserting comment. ", err, c)
 	}
 
 	c.JSON(200, comment)
 }
 
+// GetReplyCommentList returns a list consists of comments for the reply.
 func (a *Api) GetReplyCommentList(c *gin.Context) {
-	c.JSON(200, "okok")
+	query := c.Request.URL.Query()
+	query.Set("reply_id", c.Param("reply_id"))
+	list, err := a.Store.ReplyCommentList(query)
+
+	if err != nil {
+		JSONBadRequestError("Error in getting reply comment list. ", err, c)
+	}
+
+	c.JSON(200, list)
 }
 
+// PostReplyComment creates a comment for the reply.
 func (a *Api) PostReplyComment(c *gin.Context) {
-	c.JSON(200, "okok")
+	// claims := jwt.ExtractClaims(c)
+	in := new(model.Comment)
+	err := c.ShouldBind(in)
+
+	if err != nil {
+		JSONBadRequestError("Error in binding comment. ", err, c)
+	}
+
+	comment := new(model.Comment)
+	comment.Body = in.Body
+	// comment.AuthorID = claims["id"]
+	rid, _ := strconv.Atoi(c.Param("reply_id"))
+
+	if err = a.Store.ReplyCommentCreate(comment, rid); err != nil {
+		JSONBadRequestError("Error in inserting comment. ", err, c)
+	}
+
+	c.JSON(200, comment)
 }
--- postgres/comment.go
+++ postgres/comment.go
@@ -1,7 +1,9 @@
 package postgres
 
 import (
+	"fmt"
 	"net/url"
+	"strconv"
 
 	"github.com/go-pg/pg"
 
@@ -11,17 +13,34 @@ import (
 
 type Comments []model.Comment
 
-func (s *Store) CommentList(query url.Values) (Comments, error) {
-	var comments Comments
+func (s *Store) QuestionCommentList(query url.Values) (Comments, error) {
+	var question model.Question
+	qid, _ := strconv.Atoi(query.Get("question_id"))
+	fmt.Println(query)
+	p, err := s.pagination(&question, query, qid, "Comments")
 
-	err := s.db.Model(&comments).
-		Apply(orm.Pagination(query)).
-		Select()
-
-	return comments, err
+	return p.(*model.Question).Comments, err
 }
 
-func (s *Store) CommentCreate(c *model.Comment, question_id int) error {
+func (s *Store) ReplyCommentList(query url.Values) (Comments, error) {
+	var reply model.Reply
+	rid, _ := strconv.Atoi(query.Get("reply_id"))
+	p, err := s.pagination(&reply, query, rid, "Comments")
+
+	return p.(*model.Reply).Comments, err
+}
+
+func (s *Store) pagination(model interface{}, query url.Values, id int, relation string) (interface{}, error) {
+	err := s.db.Model(model).
+		Apply(orm.Pagination(query)).
+		Where("id = ?", id).
+		Relation(relation).
+		Select()
+
+	return model, err
+}
+
+func (s *Store) QuestionCommentCreate(c *model.Comment, question_id int) error {
 	return s.db.RunInTransaction(func(tx *pg.Tx) error {
 		err := tx.Insert(c)
 		if err != nil {
@@ -33,6 +52,26 @@ func (s *Store) CommentCreate(c *model.Comment, question_id int) error {
 		cq.QuestionId = question_id
 
 		err = tx.Insert(cq)
+		if err != nil {
+			return err
+		}
+
+		return nil
+	})
+}
+
+func (s *Store) ReplyCommentCreate(c *model.Comment, reply_id int) error {
+	return s.db.RunInTransaction(func(tx *pg.Tx) error {
+		err := tx.Insert(c)
+		if err != nil {
+			return err
+		}
+
+		cr := new(model.CommentsReply)
+		cr.CommentId = c.Id
+		cr.ReplyId = reply_id
+
+		err = tx.Insert(cr)
 		if err != nil {
 			return err
 		}
